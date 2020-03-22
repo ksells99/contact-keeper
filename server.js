ea60bed4dd18004f6309b56e09305af62f6acbab
Kai Sells <kai.sells@hotmail.co.uk>
2020-03-22 17:11:50 +0000

    Corrected server.js file

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ if(process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 };
 
-const PORT = process.eventNames.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
